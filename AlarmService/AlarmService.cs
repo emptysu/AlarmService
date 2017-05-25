ec5fdd281ccd37ee368be44f402c6c514492221d
emptysu <321349669@qq.com>
2017-05-25 14:48:37 +0800

    Add new project
    Alarm service

---
+++ AlarmService/AlarmService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlarmService
+{
+    public partial class AlarmService : ServiceBase
+    {
+        public AlarmService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
